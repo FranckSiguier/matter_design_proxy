ad538a6f837a94f07ea5d0eb584af1921d1602a7
Franck Siguier <franck.siguier@gmail.com>
2022-05-07 02:58:03 +1000

    Create the route to add a bulk pricing rule

--- proxyServer.js
+++ proxyServer.js
@@ -6,12 +6,19 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const URL = process.env.URL;
 const TOKEN = process.env.TOKEN;
+const POWERFUL_TOKEN = process.env.TOKEN_TO_MODIFY;
 
 const HEADERS = {
   "X-Auth-Token": TOKEN,
+  "Content-Type": "application/json",
+};
+
+const POWERFUL_HEADERS = {
+  "X-Auth-Token": POWERFUL_TOKEN,
   "Content-Type": "application/json",
 };
 
@@ -37,6 +44,37 @@ app.get("/products/:productId/images", async (req, res) => {
   const images = response.data.data;
 
   res.json(images);
+});
+
+app.get("/products/:productId/bulk-pricing-rules", async (req, res) => {
+  const productId = req.params.productId;
+
+  const response = await axios({
+    method: "get",
+    url: `${URL}/${productId}/bulk-pricing-rules`,
+    headers: HEADERS,
+  });
+
+  res.json(response.data);
+});
+
+app.post("/products/:productId/bulk-pricing-rules", async (req, res) => {
+  const productId = req.params.productId;
+  const { quantity_min, quantity_max, type, amount } = req.body;
+
+  const response = await axios({
+    method: "post",
+    url: `${URL}/${productId}/bulk-pricing-rules`,
+    headers: POWERFUL_HEADERS,
+    data: {
+      quantity_min,
+      quantity_max,
+      type,
+      amount,
+    },
+  });
+
+  res.json(response.data);
 });
 
 app.listen(4000, () => {
